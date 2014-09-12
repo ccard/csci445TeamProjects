4e3e9727d9b8a3add9b264fdc38abf21642d78b6
ccard <ccard@mymail.mines.edu>
2014-09-12 11:19:32 -0600

    got the changing transitions working

--- Project1/planer.js
+++ Project1/planer.js
@@ -1,112 +1,99 @@
-function animateGame(){
-  var gameDiv = document.getElementById('gamePage');
-  var aboutDiv = document.getElementById('aboutPage');
-  if(aboutDiv.style.display != "none"){
-    aboutDiv.style.display ="none";
-  }
-  if(gameDiv.style.display == "none"){
-    gameDiv.style.opacity = 0.0;
-    gameDiv.style.display = "block";
-    var opacity = 0;
-    function frame(){
-      opacity += 0.1;
-      gameDiv.style.opacity = opacity;
-      if(opacity == 1.0){
-        clearInterval(id);
-      }
-      
+function animateGame() {
+    var gameDiv = document.getElementById('gamePage');
+    var aboutDiv = document.getElementById('aboutPage');
+    var aboutStyle = window.getComputedStyle(aboutDiv, null);
+    var gameStyle = window.getComputedStyle(gameDiv, null);
+    if (aboutStyle.visibility == "visible") {
+        aboutDiv.classList.toggle('classShow');
+        aboutDiv.classList.toggle('classHide');
     }
-    var id = setInterval(frame,100);
-  }
+    if (gameStyle.visibility == "hidden") {
+        gameDiv.classList.toggle('classHide');
+        gameDiv.classList.toggle('classShow');
+    }
 }
-function animateAbout(){
-  var gameDiv = document.getElementById('gamePage');
-  var aboutDiv = document.getElementById('aboutPage');
-  if(gameDiv.style.display != "none"){
-    gameDiv.style.display ="none";
-  }
-  if(aboutDiv.style.display == "none"){
-    aboutDiv.style.opacity = 0.0;
-    aboutDiv.style.display = "block";
-    var opacity = 0;
-    function frame(){
-      opacity += 0.1;
-      aboutDiv.style.opacity = opacity;
-      if(opacity == 1.0){
-        clearInterval(id);
-      }
-      
+
+function animateAbout() {
+    var gameDiv = document.getElementById('gamePage');
+    var aboutDiv = document.getElementById('aboutPage');
+    var aboutStyle = window.getComputedStyle(aboutDiv, null);
+    var gameStyle = window.getComputedStyle(gameDiv, null);
+    if (gameStyle.visibility == "visible") {
+        gameDiv.classList.toggle('classShow');
+        gameDiv.classList.toggle('classHide');
     }
-    var id = setInterval(frame,100);
-  }
+    if (aboutStyle.visibility == "hidden") {
+        aboutDiv.classList.toggle('classHide');
+        aboutDiv.classList.toggle('classShow');
+    }
 }
 
 //TODO: init game and other functions to interact with html
 
 
 //Game controll
-function GameRunner () {
-  
-  var canvas = document.getElementById("gameArea");
-  var graphFaces = [];
+function GameRunner() {
 
-  //TODO: functions for submit and clearing 
+    var canvas = document.getElementById("gameArea");
+    var graphFaces = [];
+
+    //TODO: functions for submit and clearing 
 
 
-  function graphFace (pointList, color) {
-     
-    /*
+    function graphFace(pointList, color) {
+
+        /*
       point object = {x:<xval>, y:<val>}
       pointlist point objects and has at least three points
 
       color is expected to to be a hex number i.e. #0f00ff
     */
-    this.c = color;
-     this.points = pointList;
+        this.c = color;
+        this.points = pointList;
 
-     this.containsPoint = function (x,y,color,canvas) {
-       // TODO: check if point is in a polygon
-     };
+        this.containsPoint = function(x, y, color, canvas) {
+            // TODO: check if point is in a polygon
+        };
 
-     this.renderFace = function  (canvas) {
-       if(canvas && this.points){
-          if(this.points.length >= 3){
-            var context = canvas.getContext("2d");
-            context.save();
-            cotext.beginPath();
-            context.fillStyle = this.c;
-            context.lineWidth = 2;
-            context.strokeStyle = "#000";
-            for (var i = this.points.length - 1; i >= 0; i--) {
-              context.moveTo(this.points[i].x,this.points[i].y);
-              if (i === 0) {
-                context.lineTo(this.points[this.points.length-1].x,this.points[this.points.length-1].y);
-              } else{
-                context.lineTo(this.points[i-1].x,this.points[i-1].y);
-              }
+        this.renderFace = function(canvas) {
+            if (canvas && this.points) {
+                if (this.points.length >= 3) {
+                    var context = canvas.getContext("2d");
+                    context.save();
+                    cotext.beginPath();
+                    context.fillStyle = this.c;
+                    context.lineWidth = 2;
+                    context.strokeStyle = "#000";
+                    for (var i = this.points.length - 1; i >= 0; i--) {
+                        context.moveTo(this.points[i].x, this.points[i].y);
+                        if (i === 0) {
+                            context.lineTo(this.points[this.points.length - 1].x, this.points[this.points.length - 1].y);
+                        } else {
+                            context.lineTo(this.points[i - 1].x, this.points[i - 1].y);
+                        }
+                    }
+
+                    context.stroke();
+                    context.fill();
+                    context.closePath();
+
+                    context.fillStyle = "#000";
+                    for (var i = this.points.length - 1; i >= 0; i--) {
+                        context.beginPath();
+                        context.arc(this.points[i].x, this.points[i].y, 2, 0, 2 * Math.PI);
+                        context.stroke();
+                        context.fill();
+                        context.closePath();
+                    }
+
+                    context.restore();
+                }
             }
+        };
+    }
 
-            context.stroke();
-            context.fill();
-            context.closePath();
+    //init mouse events
 
-            context.fillStyle = "#000";
-            for (var i = this.points.length - 1; i >= 0; i--) {
-              context.beginPath();
-              context.arc(this.points[i].x,this.points[i].y,2,0,2*Math.PI);
-              context.stroke();
-              context.fill();
-              context.closePath();
-            }
-
-            context.restore();
-          }
-       }
-     };
-  }
-
-  //init mouse events
-
-  //init board by random seed
+    //init board by random seed
 
 }
