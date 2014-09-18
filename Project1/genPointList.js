ec88c4b0100455533db64ed096befca24ed5f44d
Mihughes <mihughes@mymail.mines.edu>
2014-09-18 17:48:27 -0600

    updated gen points

--- Project1/genPointList.js
+++ Project1/genPointList.js
@@ -14,38 +14,48 @@
       TODO: deal with the crossing lines.
 
     */
-onload=genPointList;
 
-var maxX = 500;  //canvas dimensions. Figure out how to get these from the HTML page.
-var maxY = 500;
-var maxPoints = 3; // including 0, this is really 4 points
 
-// 
-var relativeSize=maxX/5;  // edges will be about this long in relation to maxX
+// TODO: learn how to use closures, whack the global variables
+var maxX;  //canvas dimensions used globally 
+var maxY;
 
-var localTest = true;
+var maxPoints = 2; // including 0, add 1 to this number
+var relativeSize = 2;  // scales down the shape, smaller number gives bigger shape
+
+var localTest = true;  // for drawing on a test canvas
 
 function genPointList() {  
 	var canvas = document.getElementById("canvas");
+	canvas.addEventListener("mousedown", getPosition, false);
 
+	do {  //do loop cycles through sets of points until a good set is made
+		var prePoint = [];		//holds presorted list of points
+		var prePoint1 = [];
+		var sortedPoint = [];	//sorted by X
+		var polyPoint = [];  	//sorted by Polygon shape
+		var numPoints;			//random number of points to make
 
-	var prePoint = [];		//holds presorted list of points
-	var sortedPoint = [];
-	var numPoints;			//random number of points to make		
+		maxX = canvas.width;  //canvas dimensions. 
+		maxY = canvas.height;		
 
-	numPoints = numPointsToMake();
-	
-	// generate an unsorted list of points
-	for (i=0; i <= numPoints; i++) {
-		prePoint.push(new Point(randX(),randY()));	
-	}
-	
-	sortedPoint = prePoint;  			//for now, keep an orginal copy of the points in prePoint
-	sortedPoint.sort(sortPoints);		//sortedPoint becomes the working list
-	
-	// test the array of points. 
-	testPoints(sortedPoint);
+		numPoints = numPointsToMake();
+		
+		// generate an unsorted list of points
+		for (i=0; i <= numPoints; i++) {
+			prePoint.push(new Point(randX(),randY()));	
+		}
 
+		// sort the points in three steps
+		sortedPoint = prePoint.slice(0);  	//for now, keep an orginal copy of the points in prePoint
+		sortedPoint.sort(sortPoints);//sortedPoint becomes the working list. "sortPoints" is a local function.
+		polyPoint = polySort(sortedPoint);   // local function
+
+	} while(badGeometry(polyPoint));
+
+	// show the array of points. 
+	//testPoints(sortedPoint);  //use a sort-By-X list
+	testPoints(polyPoint);		// use a poly-sorted list 
 
 // --------- Helper functions ------------
 } 
@@ -66,23 +76,19 @@ function numPointsToMake() {
 	return rand(min,maxPoints);
 }
 
-// randomly adjust the size of the shapes, plus or minus 10 pixels
-function sizeVariance() {
-	var max=10,min=-10;
-	return rand(min,max);
-}
+
 
 //generate a random X
 function randX() {
 	var min = 0 + 10; //stay away from the edge
-	var max = maxX - 10;
+	var max = (maxX - 10)/relativeSize;
 	return rand(min, max);
 
 }
 
 function randY() {
 	var min = 0 + 10; //stay away from the edge
-	var max = maxY - 10;
+	var max = (maxY - 10)/relativeSize;
 	return rand(min, max);
 
 }
@@ -105,21 +111,98 @@ function sortPoints(a,b) {
 	if (a.x > b.x)
 		return 1;
 	return 0;
+}
 
+function polySort(inList) {
+	//TODO: still got some line crosses
+	// issues: not solving for multiple minX and multiple maxX
+	
+	var tempList = inList.slice(0);   //clone a working copy
+	
+	var pointMinX, pointMaxX;
+	var pointMinY, pointMaxY;
+	var polyPt = [];    			//output array
+
+	//find pointMinX
+	pointMinX = tempList[0].x;
+	pointMaxX = tempList[tempList.length-1].x
+
+	// we know point 0 is root, so pop it to the poly sort list.
+	// TODO: it might be better to use a middle X point.
+	polyPt.push(new Point(tempList[0].x, tempList[0].y));
+	tempList.splice(0,1); //pop point 0
+
+	// move along x, look for Y <= point0.y, until maxX is reached
+	for (var index =0, l = tempList.length; index < l; index++){
+		if(tempList[index].y <= polyPt[0].y){
+			polyPt.push(new Point(tempList[index].x, tempList[index].y));
+		}
+	}
+
+	// Now move left, look for Y > point0.y, until at head of list
+	//alert("tempList.length-1: " + ((tempList.length)-1));
+	for(var index = ((tempList.length)-1);  index >= 0; index--) {
+
+		if(tempList[index].y > polyPt[0].y) {
+			polyPt.push(new Point(tempList[index].x, tempList[index].y));
+		}
+	}
+
+	return polyPt;
+}
+
+function badGeometry(points) {
+	
+// 	var r1; var r2; var r3;
+// 	var good = false;
+// 	var x0=points[0].x; var y0=points[0].y;
+// 	var x1=points[1].x; var y1=points[1].y;
+// 	var x2=points[2].x; var y2=points[2].y;
+
+// 	var lenOne   = Math.sqrt(    Math.pow( ( x1 - x0 ), 2 ) + Math.pow( ( y1 - y0 ), 2 )   );
+// 	var lenTwo   = Math.sqrt(    Math.pow( ( x2 - x1 ), 2 ) + Math.pow( ( y2 - y1 ), 2 )   );
+// 	var lenThree = Math.sqrt(    Math.pow( ( x0 - x2 ), 2 ) + Math.pow( ( y0 - y2 ), 2 )   );
+// //if lenOne,  and lenTwo  and lenThree are within 50% of each other 
+// 	r1 = lenOne / lenTwo;
+// 	r2 = lenTwo / lenThree;
+// 	r3 = lenThree / lenOne;
+// if(       (r1 > .5) && (r1 < 2) && (r2 > .5) && (r2 < 2) && (r3 > .5) && (r3 < 2)                              ) {
+// 	return true;
+// }
+
+//alert(" Line Length  1: " + lenOne.toFixed(0) + ", \n\r Line Length  2: " + lenTwo.toFixed(0) + ", \n\r Line Length  3; " + lenThree.toFixed(0) + ",\n\r Acceptable set?  " + good);
+
+	return false;
 
 }
 
+function clearCanvas(context, canvas) {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  var w = canvas.width;
+  canvas.width = 1;
+  canvas.width = w;
+}   
+
+function morethings() {  //have not tried these functions yet
+
+	ctx.rotate(radians*Math.PI/180);
+	ctx.translate(70,70);
+}
+
 // draw points and info on the screen to see how they look.
-function testPoints(point) {
+function testPoints(point, polyPoint) {
 	if(localTest) {
+		
 		if(canvas.getContext)
 		{
+			
 			var ctx = canvas.getContext("2d");
 
+			clearCanvas(ctx, canvas);
 			// Draw shape
 			ctx.fillStyle=randColor();  
 			ctx.lineWidth=6; 
-			ctx.strokeStyle="#000000"; //color of all lines
+			ctx.strokeStyle="#FFFFFF"; //color of all lines
 			ctx.beginPath();
 
 			// Draw the first point of the shape
@@ -136,15 +219,16 @@ function testPoints(point) {
 			ctx.stroke();  //draw outline
 
 			//show point numbers
-			ctx.fillStyle="#000000"; 
+			ctx.fillStyle="#FFFFFF"; 
 			//https://developer.mozilla.org/en-US/docs/Drawing_text_using_a_canvas#fillText%28%29
+
 			ctx.font = "20pt Arial"
 			for(i = 0; i < point.length; i++) {
 				ctx.fillText("Point " + (i +1) + " x: " + point[i].x + ", y: " + point[i].y, 500, (i + 1) * 50);			
 
 			}
 
-			//draw a scale in black
+			//draw a scale in white
 			ctx.moveTo(10,10);
 			ctx.lineTo(10,700);
 			ctx.lineTo(770,700);
@@ -153,12 +237,30 @@ function testPoints(point) {
 
 			ctx.fillText("0,0", 20, 20);
 			ctx.fillText("0,700", 20, 700);
-			ctx.fillText("770,700", 650, 700);		
+			ctx.fillText("770,700", 650, 700);	
+
+			document.getElementById("status").innerHTML = "Was a cross detected?"
 		}
 	}
+
+
 // end of local test	
 
 }
 
+// eventhandlers
+canvas.addEventListener("mousedown", getPosition, false);
+
+function getPosition(event) {
+
+  var x = event.x;
+  var y = event.y;
+
+   x -= canvas.offsetLeft;
+  y -= canvas.offsetTop;
+
+  alert("x:" + x + " y:" + y);
+
+}
 
 
