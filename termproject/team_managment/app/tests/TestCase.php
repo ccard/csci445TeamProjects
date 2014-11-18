a8491218d50c8c2d8ce397f544593386efb86104
Mihughes <mihughes@mymail.mines.edu>
2014-11-17 22:33:17 -0700

    fixed phpunit tests

--- termproject/team_managment/app/tests/TestCase.php
+++ termproject/team_managment/app/tests/TestCase.php
@@ -2,6 +2,13 @@
 
 class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
+	public function setUp() { // this one must run before the rest of the DB tests
+			// it loads the database into memory, but not the hard disk.
+		parent::setUp();
+		$this->prepareForTests();
+
+	}	
+
 	/**
 	 * Creates the application.
 	 *
@@ -15,5 +22,13 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
 		return require __DIR__.'/../../bootstrap/start.php';
 	}
+
+	public function prepareForTests() //http://culttt.com/2013/05/20/getting-started-with-testing-laravel-4-models/
+	{
+  		Artisan::call('migrate');
+		Mail::pretend(true);
+		$this->seed();     // this is written to memory, not production.sqlite.
+		//echo ' End of Setup()  ';
+	}	
 
 }
