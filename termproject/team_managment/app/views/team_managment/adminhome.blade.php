758464460de3b54f0a3143f3519bfa228fb45e67
ccard <ccard@mymail.mines.edu>
2014-12-01 16:20:34 -0700

    fixed a error

--- termproject/team_managment/app/views/team_managment/adminhome.blade.php
+++ termproject/team_managment/app/views/team_managment/adminhome.blade.php
@@ -23,7 +23,7 @@
 			<div class="alert alert-warning">
 				{{ Form::open(array('action'=>'GenerateTeams@generateTeams')) }}
 				Number of students: {{(User::count())-1}}<br>
-				Number of students that have submitted preferences: {{(projectpreferences::count())-1}}<br>
+				Number of students that have submitted preferences: {{(ProjectPreferences::count())-1}}<br>
 				If enough students have have posted preferences, please generate tables by clicking {{ Form::submit('here',array("class"=>"btn-link")) }}
 				{{ Form::close() }}
 			</div>
